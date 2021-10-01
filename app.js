1dee4c501142fdf12995b3dae1b98d59e5712e79
yuraoliynyk <yuraoliynyk.20@gmail.com>
2021-10-01 18:04:51 +0300

    hw-1: completed

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const path = require("path");
-const tools = require("./tools");
+const tools = require("./tools/tools");
 
 const boysPath = path.join(__dirname, "boys");
 const girlsPath = path.join(__dirname, "girls");
 
+const workPath = path.join(__dirname, "dir-0");
 
-tools.sortFilesJSON(boysPath, "gender","male", girlsPath);
-tools.sortFilesJSON(girlsPath, "gender", "female", boysPath);
+
+tools.filesSort(boysPath, "gender","male", girlsPath);
+tools.filesSort(girlsPath, "gender", "female", boysPath);
+
+tools.filesUpTo(workPath, workPath);
---
+++ tools/tools.js
@@ -0,0 +1,89 @@
+const fs = require("fs");
+const path = require("path");
+
+
+const fileMove = (fileName, fileDirPath, moveDirPath) => {
+    if (fileDirPath === moveDirPath) {
+        console.log(`***  File "${fileName}" not moved. It is already here`);
+        return;
+    }
+
+    fs.rename(
+        path.join(fileDirPath, fileName),
+        path.join(moveDirPath, fileName),
+
+        (err) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+
+            console.log(`***  File "${fileName}" was moved`);
+        }
+    );
+};
+
+
+const filesSort = (xPath, xPropertyName, xPropertyValue, yPath) => {
+    fs.readdir(xPath, (err, files) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        files.forEach(fileName => {
+            fs.readFile(path.join(xPath, fileName), (err, fileData) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+
+                const fileObj = JSON.parse(fileData.toString());
+
+                if (fileObj[xPropertyName] !== xPropertyValue) {
+                    fileMove(fileName, xPath, yPath);
+                }
+            });
+        });
+    });
+};
+
+
+const filesUpTo = (upPath, scanPath) => {
+    fs.readdir(scanPath, (err, elements) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        if (elements.length === 0) {
+            return;
+        }
+
+        elements.forEach(element => {
+            const elementPath = path.join(scanPath, element);
+
+            fs.stat(elementPath, (err, stats) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+
+                if (stats.isFile() === true) {
+                    if (scanPath !== upPath) {
+                        fileMove(element, scanPath, upPath);
+                    }
+                } else {
+                    filesUpTo(upPath, elementPath);
+                }
+            });
+        });
+    });
+};
+
+
+module.exports = {
+    fileMove,
+    filesSort,
+    filesUpTo
+};
